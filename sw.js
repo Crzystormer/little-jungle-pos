9c0e5a0fd5ab2e40eee8917a6b86c789d9e2f3b2
Crzystormer <willem@rettup.co.za>
2025-10-23 12:46:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pos-cache-v1').then(cache => {
+      return cache.addAll([
+        './Grok1.html',
+        'https://unpkg.com/react@18/umd/react.production.min.js',
+        'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+        'https://unpkg.com/@babel/standalone/babel.min.js',
+        'https://cdn.tailwindcss.com',
+        'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+        'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore-compat.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
